d8eb9da6caa757155e284e21182ce734ce4d3de5
do02reen24 <do02reen24@gmail.com>
2020-11-24 13:54:55 +0900

    feat : 파일 경로 설정 및 color theme 생성 - 자주 사용되는 color를 정의함 - 파일 경로를 단순화하게 하기위해 설정해줌

---
+++ client/craco.config.js
@@ -0,0 +1,13 @@
+import CracoAlias from 'craco-alias';
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+};
---
+++ client/src/theme/color.ts
@@ -0,0 +1,31 @@
+interface Color {
+  [key: string]: string;
+}
+
+interface Colors {
+  [key: string]: Color;
+}
+
+const myColor: Colors = {
+  primary: {
+    dark: '#FFB421',
+    main: '#F4C239',
+    light: '#FFE6A0',
+    accent: '#7392FF',
+    reject: '#FF7373',
+    cancel: '#C4C4C4',
+  },
+  money: {
+    expenditure: '#7392FF',
+    income: '#F4C239',
+  },
+  header: {
+    dark: '#7A5A00',
+    light: '#FFFFFF',
+  },
+  background: {
+    lightGray: '#EEEEEE',
+  },
+};
+
+export default myColor;
