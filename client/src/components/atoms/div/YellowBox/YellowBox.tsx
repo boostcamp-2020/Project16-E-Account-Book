71232b899a04cc61f1a383180c14e71bd56e1612
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-11-26 03:42:01 +0900

    fix : yellowbox width 버그 해결

--- client/src/components/atoms/div/YellowBox/YellowBox.tsx
+++ client/src/components/atoms/div/YellowBox/YellowBox.tsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 
 const YellowBox = styled.div`
   position: relative;
+  width: 100vw;
+  height: 100vh;
 `;
 
 const InBox = styled.div`
   position: absolute;
-  width: 30vw;
+  width: 100%;
   height: 0;
   border-right: 35vw solid transparent;
   border-top: 30vw solid #f4c238;
