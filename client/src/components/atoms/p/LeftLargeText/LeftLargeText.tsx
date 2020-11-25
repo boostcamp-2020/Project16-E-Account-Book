966c1a6bd3938e3c95e8610fcf10839beab29844
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-11-25 16:45:20 +0900

    feat : LeftLargeText 생성
    - tsx 요소 생성
    - props (default)
      - fontSize (24px)
      - fontWeight (normal)
      - color (color.primary.kakaoBlack)
      - text-align (left)

---
+++ client/src/components/atoms/p/LeftLargeText/LeftLargeText.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import myColor from '@theme/color';
+
+interface Props extends TextProps {
+  children: React.ReactChild;
+}
+
+interface TextProps {
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
+}
+
+const defaultProps = {
+  color: myColor.primary.kakaoBlack,
+  fontSize: '24px',
+  fontWeight: 'normal',
+};
+
+const Text = styled.p<TextProps>`
+  font-size: ${(props) => props.fontSize};
+  background-color: transparent;
+  color: ${(props) => props.color};
+  fontweight: ${(props) => props.fontWeight};
+  text-align: left;
+`;
+
+const LeftLargeText: React.FC<Props> = ({ color, fontSize, fontWeight, children }: Props) => {
+  return (
+    <Text color={color} fontSize={fontSize} fontWeight={fontWeight}>
+      {children}
+    </Text>
+  );
+};
+
+LeftLargeText.defaultProps = defaultProps;
+
+export default LeftLargeText;
