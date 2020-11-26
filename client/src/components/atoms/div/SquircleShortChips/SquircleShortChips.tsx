e68bdf545cb036ef3c31c42a2676488beddcf770
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-11-26 11:24:54 +0900

    fix : props 요소 추가
    - width, height, fontSize
    - 각각의 defaultProps value 지정

--- client/src/components/atoms/div/SquircleShortChips/SquircleShortChips.tsx
+++ client/src/components/atoms/div/SquircleShortChips/SquircleShortChips.tsx
@@ -9,11 +9,17 @@ interface Props extends ChipsProps {
 interface ChipsProps {
   backgroundColor?: string;
   color?: string;
+  width?: string;
+  height?: string;
+  fontSize?: string;
 }
 
 const defaultProps = {
   color: myColor.primary.accent,
   backgroundColor: myColor.primary.lightGray,
+  width: '3rem',
+  height: '1.2rem',
+  fontSize: '0.3rem',
 };
 
 const Chips = styled.div<ChipsProps>`
@@ -21,17 +27,30 @@ const Chips = styled.div<ChipsProps>`
   flex-flow: column;
   justify-content: center;
   text-align: center;
-  font-size: 0.3rem;
-  width: 3rem;
-  height: 1.2rem;
+  font-size: ${(props) => props.fontSize};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   background-color: ${(props) => props.backgroundColor};
   color: ${(props) => props.color};
   border-radius: 3px;
 `;
 
-const SquircleShortChips: React.FC<Props> = ({ backgroundColor, color, children }: Props) => {
+const SquircleShortChips: React.FC<Props> = ({
+  backgroundColor,
+  width,
+  height,
+  fontSize,
+  color,
+  children,
+}: Props) => {
   return (
-    <Chips backgroundColor={backgroundColor} color={color}>
+    <Chips
+      backgroundColor={backgroundColor}
+      width={width}
+      height={height}
+      fontSize={fontSize}
+      color={color}
+    >
       {children}
     </Chips>
   );
