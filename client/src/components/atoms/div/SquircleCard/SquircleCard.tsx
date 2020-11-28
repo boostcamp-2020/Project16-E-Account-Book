33866e7fbee2a498ec6e65df8461afdd7c980943
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-11-28 14:45:19 +0900

    refactor : atoms/div/SquircleCard
    - flex-flow의 방향을 변경할 수 있도록 변경

--- client/src/components/atoms/div/SquircleCard/SquircleCard.tsx
+++ client/src/components/atoms/div/SquircleCard/SquircleCard.tsx
@@ -7,17 +7,19 @@ interface Props {
   width?: string;
   height?: string;
   children: React.ReactChild | React.ReactChild[];
+  flexFlow?: string;
 }
 
 const defaultProps = {
   backgroundColor: myColor.primary.main,
   width: '500px',
   height: '120px',
+  flexFlow: 'row',
 };
 
 const SquircleCard = styled.div<Props>`
   display: flex;
-  flex-flow: row;
+  flex-flow: ${(props) => props.flexFlow};
   background-color: ${(props) => props.backgroundColor};
   width: ${(props) => props.width};
   max-width: 1200px;
@@ -26,9 +28,20 @@ const SquircleCard = styled.div<Props>`
   box-shadow: 0px 5px 7px 1px ${myColor.primary.gray};
 `;
 
-const squircleCard: React.FC<Props> = ({ backgroundColor, width, height, children }: Props) => {
+const squircleCard: React.FC<Props> = ({
+  backgroundColor,
+  width,
+  height,
+  children,
+  flexFlow,
+}: Props) => {
   return (
-    <SquircleCard backgroundColor={backgroundColor} width={width} height={height}>
+    <SquircleCard
+      backgroundColor={backgroundColor}
+      width={width}
+      height={height}
+      flexFlow={flexFlow}
+    >
       {children}
     </SquircleCard>
   );
