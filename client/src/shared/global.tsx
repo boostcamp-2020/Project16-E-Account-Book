e59b93c1ff4f70618a8836ac82482e9a81a9aa4b
do02reen24 <do02reen24@gmail.com>
2020-11-29 22:52:33 +0900

    feat : 글씨체 적용 - 프론트엔드 전체에 에스코어드림Reg 글씨체를 적용함

--- client/src/shared/global.tsx
+++ client/src/shared/global.tsx
@@ -1,15 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'S-CoreDream-4Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-4Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   html, body {
     margin: 0px;
     padding: 0px;
     width: 100%;
     height: 100%;
+    font-family: 'S-CoreDream-4Regular';
     *:focus { outline:none; }
   }
+
   p{
     margin: 0px;
+    font-family: 'S-CoreDream-4Regular';
+  }
+
+  button{
+    font-family: 'S-CoreDream-4Regular';
   }
 `;
 
