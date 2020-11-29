0b2033afd800afa55e18a22306f57afcc8d9ab0a
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-11-29 16:42:02 +0900

    fix : atoms/oauthButtons, molecules/oauthButtonList
    - a 태그로 폼 변경

--- client/src/components/atoms/button/OAuthButton/OAuthButton.stories.tsx
+++ client/src/components/atoms/button/OAuthButton/OAuthButton.stories.tsx
@@ -6,25 +6,23 @@ export default {
   component: OAuthButton,
 };
 
-const onClick = (): boolean => {
-  return true;
-};
-
 export const githubButton = (): JSX.Element => {
+  const link = 'https://github.com/';
   const oauthSite = 'GitHub';
   const backgroundColor = '#444444';
   return (
-    <OAuthButton site={oauthSite} backgroundColor={backgroundColor} onClick={onClick}>
+    <OAuthButton site={oauthSite} backgroundColor={backgroundColor} link={link}>
       깃허브 계정으로 로그인
     </OAuthButton>
   );
 };
 
 export const naverButton = (): JSX.Element => {
+  const link = 'https://www.naver.com/';
   const oauthSite = 'Naver';
   const backgroundColor = '#4fa42b';
   return (
-    <OAuthButton site={oauthSite} backgroundColor={backgroundColor} onClick={onClick}>
+    <OAuthButton site={oauthSite} backgroundColor={backgroundColor} link={link}>
       네이버 계정으로 로그인
     </OAuthButton>
   );
