593bfb8b1d1c275f18a6872a90ee56090b161cc0
JunYoung <vnslt0152@gmail.com>
2020-12-01 01:46:11 +0900

    refactor : atoms 요소들 storybook control 추가
    - storybook에서의 속성 control을 위한 stories 파일 수정 ( control 추가 )
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- client/src/components/atoms/hr/Line/Line.stories.tsx
+++ client/src/components/atoms/hr/Line/Line.stories.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import myColor from '@theme/color';
 import Line from './Line';
+
+interface Props {
+  lineColor?: string;
+  widthPercent?: number;
+}
 
 export default {
   title: 'Atoms/hr/Line',
   component: Line,
+  argTypes: {
+    lineColor: { control: 'color' },
+    widthPercent: { control: 'number' },
+  },
 };
 
-export const iconButton = (): JSX.Element => {
-  const widthPercent = 50;
-  return <Line widthPercent={widthPercent} />;
+export const iconButton = ({ ...args }: Props): JSX.Element => {
+  return <Line {...args} />;
 };
 
 iconButton.story = {
   name: 'Line',
+};
+
+iconButton.args = {
+  lineColor: myColor.primary.lightGray,
+  widthPercent: 80,
 };
--- client/src/components/atoms/p/CenterSmallText/CenterSmallText.stories.tsx
+++ client/src/components/atoms/p/CenterSmallText/CenterSmallText.stories.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import myColor from '@theme/color';
 import CenterSmallText from './CenterSmallText';
+
+interface Props {
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
+}
 
 export default {
   title: 'Atoms/p/CenterSmallText',
   component: CenterSmallText,
+  argTypes: {
+    color: { control: 'color' },
+    fontSize: { control: 'text' },
+    fontWeight: { control: 'text' },
+  },
 };
 
-export const centerSmallText = (): JSX.Element => {
-  return <CenterSmallText>Sample Text</CenterSmallText>;
+export const centerSmallText = ({ ...args }: Props): JSX.Element => {
+  return <CenterSmallText {...args}>Sample Text</CenterSmallText>;
 };
 
 centerSmallText.story = {
   name: 'CenterSmallText',
+};
+
+centerSmallText.args = {
+  color: myColor.primary.black,
+  fontSize: '12px',
+  fontWeight: 'normal',
 };
