154bece51d814ca725854cc4fe56a431f4f65245
do02reen24 <do02reen24@gmail.com>
2020-11-30 20:50:50 +0900

    feat : MyPageUserMenu organisms 개발
    - MyPage의 상단 유저 정보를 구성하는 Menu를 구현함
    - 로그아웃버튼은 우측에 떨어져 위치하도록 함
    - LeftNormalText의 children을 여러개 받을 수 있도록 하고 오타를 수정함

---
+++ client/src/components/organisms/MyPageUserMenu/MyPageUserMenu.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
+import RowFlexContainer from '@atoms/div/RowFlexContainer';
+import UserImage from '@atoms/img/UserImage';
+import Logout from '@atoms/button/TextButton';
+import Span from '@atoms/span/BoldSpan';
+import P from '@atoms/p/LeftNormalText';
+import myColor from '@theme/color';
+
+interface Props {
+  name: string;
+  profile: string;
+}
+
+const ButtonContainer = styled.div`
+  margin-left: auto;
+`;
+
+const OAuthButtonList: React.FC<Props> = ({ name, profile }: Props) => {
+  return (
+    <RowFlexContainer>
+      <UserImage link={profile} />
+      <ColumnFlexContainer alignItems="flex-start" margin="0 20px">
+        <P>
+          <Span>{name}</Span> 님
+        </P>
+        <P>안녕하세요</P>
+      </ColumnFlexContainer>
+      <ButtonContainer>
+        <Logout color={myColor.primary.black}>
+          <Span>로그아웃</Span>
+        </Logout>
+      </ButtonContainer>
+    </RowFlexContainer>
+  );
+};
+
+export default OAuthButtonList;
