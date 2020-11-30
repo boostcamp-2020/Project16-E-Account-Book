286ae5ac993aa030be4ccf6a7a4dc06c91519e37
do02reen24 <do02reen24@gmail.com>
2020-11-30 19:10:13 +0900

    feat : MyPageMenu organisms 개발 - mypage 메뉴 UI를 개발함 - HexagonButton의 클릭 영역 범위를 개선함

--- client/src/components/atoms/button/HexagonButton/HexagonButton.tsx
+++ client/src/components/atoms/button/HexagonButton/HexagonButton.tsx
@@ -19,11 +19,24 @@ const defaultProps = {
   color: myColor.primary.main,
 };
 
-const Button = styled.button`
+const Container = styled.div<SizeProps>`
   position: relative;
-  margin: 0px;
-  padding: 0px;
-  border: 0px;
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  margin-left: -8px;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 3;
+  margin: 5%;
+  padding: 0;
+  border: 0;
+  width: 90%;
+  height: 90%;
+  border-radius: 50%;
   background-color: transparent;
   cursor: pointer;
 `;
@@ -49,15 +62,15 @@ const ButtonContent = styled.div<SizeProps>`
 `;
 
 const ButtonName = styled.p`
-  width: ${(props) => props.size};
-  padding: 2% 24%;
+  padding: 2% 22%;
   color: ${myColor.primary.brown};
+  text-align: center;
   font-size: 1.6em;
 `;
 
 const CreateButton: React.FC<Props> = ({ size, name, color, svgIcon, onClick }: Props) => {
   return (
-    <Button onClick={onClick}>
+    <Container size={size}>
       <ButtonBackground>
         <HexagonSVG fill={color} width={size} height={size} />
       </ButtonBackground>
@@ -65,7 +78,8 @@ const CreateButton: React.FC<Props> = ({ size, name, color, svgIcon, onClick }: 
         <ButtonName>{name}</ButtonName>
         {svgIcon}
       </ButtonContent>
-    </Button>
+      <Button onClick={onClick} />
+    </Container>
   );
 };
 
