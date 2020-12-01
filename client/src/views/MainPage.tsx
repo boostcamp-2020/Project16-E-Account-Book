08f03bf9d9d1f404aa521ea4bd77d2ce34111972
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-12-01 18:20:34 +0900

    feat : mainpage social account book 추가 Map function 구현

--- client/src/views/MainPage.tsx
+++ client/src/views/MainPage.tsx
@@ -11,6 +11,7 @@ import RowFlexContainer from '@atoms/div/RowFlexContainer';
 import CreateButton from '@atoms/button/CreateButton';
 import LeftNormalText from '@atoms/p/LeftNormalText';
 import LeftLargeText from '@atoms/p/LeftLargeText';
+import SocialAccountBook from '@organisms/SocialAccountBook';
 
 interface props {
   location: any;
@@ -37,6 +38,32 @@ const MainPage: React.FC<props> = ({ location }: props) => {
     link:
       'https://avatars2.githubusercontent.com/u/46099115?s=460&u=1e04610d430875d8189d2b212b8c2d9fc268b9db&v=4',
   };
+
+  const SocialArgs = [
+    {
+      links: [
+        'https://avatars2.githubusercontent.com/u/46099115?s=460&u=1e04610d430875d8189d2b212b8c2d9fc268b9db&v=4',
+        'https://avatars3.githubusercontent.com/u/55074799?s=460&u=2f70319c2f55ba5e26db060ba21d66a9cab35732&v=4',
+        'https://avatars2.githubusercontent.com/u/50297117?s=460&u=2ddc78ef0045b75f6fb405f1763304a7481d46e4&v=4',
+      ],
+      title: '부캠가계부',
+      description: '부스트캠프 가계부 입니다',
+      fontSize: '15px',
+      inMoney: 102349,
+      exMoney: 9932,
+    },
+    {
+      links: [
+        'https://avatars2.githubusercontent.com/u/46099115?s=460&u=1e04610d430875d8189d2b212b8c2d9fc268b9db&v=4',
+        'https://avatars3.githubusercontent.com/u/55074799?s=460&u=2f70319c2f55ba5e26db060ba21d66a9cab35732&v=4',
+      ],
+      title: '부캠가계부',
+      description: '부스트캠프 가계부 입니다',
+      fontSize: '15px',
+      inMoney: 314234,
+      exMoney: 838866,
+    },
+  ];
 
   const MarginBox = styled.div`
     width: 100%;
@@ -80,10 +107,9 @@ const MainPage: React.FC<props> = ({ location }: props) => {
           </AccountBookBackground>
           <ColumFlexContainer width="90%">
             <CardBox>
-              <MyAccountInfoCard {...chipsArgs} />
-              <MyAccountInfoCard {...chipsArgs} />
-              <MyAccountInfoCard {...chipsArgs} />
-              <MyAccountInfoCard {...chipsArgs} />
+              {SocialArgs.map((ele) => {
+                return <SocialAccountBook {...ele} />;
+              })}
             </CardBox>
           </ColumFlexContainer>
         </ColumFlexContainer>
