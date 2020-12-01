b4b766c7d8186ad7d240175c988400d45808c1eb
JunYoung <vnslt0152@gmail.com>
2020-12-01 18:48:25 +0900

    refactor : CreateAccountbookFromBox 수정
    - 시각적 UI 개선을 위해 겉을 감싸는 부분을 Container로 지정하지 않고 SquircleCard로써 구현
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- client/src/components/organisms/CreateAccountbookFormBox/CreateAccountbookFormBox.tsx
+++ client/src/components/organisms/CreateAccountbookFormBox/CreateAccountbookFormBox.tsx
@@ -3,8 +3,8 @@ import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
 import RowFlexContainer from '@atoms/div/RowFlexContainer';
 import LeftNormalText from '@atoms/p/LeftNormalText';
 import TextArea from '@atoms/textarea/TextArea';
-import styled from 'styled-components';
 import myColor from '@theme/color';
+import SquircleCard from '@atoms/div/SquircleCard';
 
 interface Props {
   backgroundColor?: string;
@@ -14,19 +14,20 @@ const defaultProps = {
   backgroundColor: myColor.primary.main,
 };
 
-const FormBoxContainer = styled.div<Props>`
-  background-color: ${(props) => props.backgroundColor};
-`;
-
 const CreateAccountbookFormBox: React.FC<Props> = ({ backgroundColor }: Props) => {
   return (
-    <FormBoxContainer backgroundColor={backgroundColor}>
-      <ColumnFlexContainer margin="2rem 0 0 0">
+    <SquircleCard width="100%" backgroundColor={backgroundColor} height="25rem">
+      <ColumnFlexContainer
+        width="100%"
+        height="100%"
+        justifyContent="space-around"
+        margin="2rem 0 0 0"
+      >
         <RowFlexContainer>가계부 생성, 버튼</RowFlexContainer>
         <LeftNormalText>이름을 입력해주세요.</LeftNormalText>
-        <TextArea width="100%" height="100%" />
+        <TextArea width="80%" height="30%" />
       </ColumnFlexContainer>
-    </FormBoxContainer>
+    </SquircleCard>
   );
 };
 
