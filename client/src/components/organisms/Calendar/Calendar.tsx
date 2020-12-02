c13c317c451440b4fd07824b205e9e29e5bcf013
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-12-02 21:36:28 +0900

    feat : Calender organisms 파일 생성

---
+++ client/src/components/organisms/Calendar/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Calendar = styled.div`
+  border: 1px solid red;
+`;
+const calendar: React.FC = () => {
+  return <Calendar />;
+};
+
+export default calendar;
