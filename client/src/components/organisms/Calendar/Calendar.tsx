865bab5fa448bccaa2fb4aa88c8685f396780657
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-12-02 22:20:45 +0900

    feat : isLeafYear 윤년확인 util 함수 구현

--- client/src/components/organisms/Calendar/Calendar.tsx
+++ client/src/components/organisms/Calendar/Calendar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import firstDayOfWeek from '@utils/fristDayOfWeek';
+import isLeafYear from '@utils/isLeapYear';
 
 const Calendar = styled.div`
   border: 1px solid red;
 `;
 const calendar: React.FC = () => {
   const date = firstDayOfWeek('2020-12');
-  return <Calendar>{`${date}`}</Calendar>;
+  const leafyear = isLeafYear(2020);
+  return <Calendar>{`${date}${leafyear}`}</Calendar>;
 };
 
 export default calendar;
