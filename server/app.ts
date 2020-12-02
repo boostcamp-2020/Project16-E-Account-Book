147658ff3245ec61db828a51cf26d836a92b4c48
do02reen24 <do02reen24@gmail.com>
2020-12-02 23:05:14 +0900

    fix : verifyToken header 방식으로 변경
    - 기존의 body로 부터 token값을 가져와 검증하던 과정을 header를 통해 진행하도록 변경함
    - return 시 401 status를 추가해줌

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,18 @@
 import { Context } from 'koa';
 import 'dotenv/config';
 import oauthRouter from './src/oauth/router';
+import socialBookRouter from './src/socialBook/router';
+import verifyToken from './src/middleware/verifyToken';
 
 const Koa = require('koa');
-const Router = require('@koa/router');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 
-const router = new Router();
 const app = new Koa();
 
 app.listen(3000);
 app.use(cors());
 app.use(bodyParser());
 app.use(oauthRouter.routes());
-app.use(router.routes());
+app.use(verifyToken);
+app.use(socialBookRouter.routes());
