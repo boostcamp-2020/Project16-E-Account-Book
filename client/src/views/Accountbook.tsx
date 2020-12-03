a978e305d22620cfb3a4423008bfb0d5d005fcbb
leejiwoo <leejiwoo2002@kyoggi.ac.kr>
2020-12-03 10:49:03 +0900

    fix : useState 호출 변경

--- client/src/views/Accountbook.tsx
+++ client/src/views/Accountbook.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
@@ -21,7 +21,7 @@ const useStyles = makeStyles({
 
 const Accountbook: React.FC = () => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event: React.ChangeEvent<any>, newValue: number) => {
     setValue(newValue);
