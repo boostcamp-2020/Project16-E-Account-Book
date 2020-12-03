1101110ab1505a0c2ef9d30708c5f2ca0e2bbcdc
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-12-03 18:32:17 +0900

    feat : Molecules CheckBoxWithNumber 구현

--- client/src/components/molecules/CheckBoxWithNumber/CheckBoxWithNumber.stories.tsx
+++ client/src/components/molecules/CheckBoxWithNumber/CheckBoxWithNumber.stories.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Color from '@theme/color';
 import CheckBoxWithNumber from './CheckBoxWithNumber';
 
 export default {
@@ -7,7 +8,21 @@ export default {
 };
 
 export const checkBoxWithNumber = (): JSX.Element => {
-  return <CheckBoxWithNumber />;
+  const [check, setCheck] = useState(false);
+  const fontWeight = 'bold';
+  const fontSize = '15px';
+  const money = 12600;
+  return (
+    <CheckBoxWithNumber
+      checked={check}
+      color={Color.money.income}
+      description="수입"
+      onClick={() => setCheck(!check)}
+      fontWeight={fontWeight}
+      fontSize={fontSize}
+      money={money}
+    />
+  );
 };
 
 checkBoxWithNumber.story = {
--- client/src/components/molecules/CheckBoxWithNumber/CheckBoxWithNumber.tsx
+++ client/src/components/molecules/CheckBoxWithNumber/CheckBoxWithNumber.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import CheckBox from '@atoms/checkbox/CheckBox';
+import NumberText from '@atoms/p/NumberText';
 
-const checkBoxWithNumber: React.FC = () => {
-  return <CheckBoxWithNumber />;
+interface Props {
+  onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  checked: boolean;
+  color: string;
+  description: string;
+  fontWeight: string;
+  fontSize: string;
+  money: number;
+}
+
+const CheckBoxWithNumber = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 80px;
+  height: 50px;
+`;
+
+const checkBoxWithNumber: React.FC<Props> = ({
+  onClick,
+  checked,
+  description,
+  color,
+  fontWeight,
+  fontSize,
+  money,
+}: Props) => {
+  return (
+    <CheckBoxWithNumber>
+      <CheckBox description={description} color={color} checked={checked} onClick={onClick} />
+      <NumberText fontWeight={fontWeight} fontSize={fontSize} color={color} money={money} />
+    </CheckBoxWithNumber>
+  );
 };
-
-// CheckBoxWithNumber.defaultProps = defaultProps;
 
 export default checkBoxWithNumber;
