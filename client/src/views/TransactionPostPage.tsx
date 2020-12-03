f65b1a29e3148e88f0b28a8416b25739918e32cc
do02reen24 <do02reen24@gmail.com>
2020-12-03 22:13:44 +0900

    feat : transaction 추가 페이지 UI 구축
    - TransactionForm의 경우 다른 UI를 깨트리는 문제가 있어 주석처리함

--- client/src/views/TransactionPostPage.tsx
+++ client/src/views/TransactionPostPage.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import TopNavBar from '@organisms/TopNavBar';
+import CenterContent from '@molecules/CenterContent';
+import CreateTransactionHeader from '@organisms/CreateTransactionHeader';
+// import TransactionForm from '@organisms/TransactionForm';
+import CreateTransactionMenu from '@organisms/CreateTransactionMenu';
 
 const TransactionPostPage: React.FC = () => {
+  const onClick = () => {
+    return true;
+  };
+
   return (
     <>
-      <p>transaction post page</p>
+      <CenterContent>
+        <TopNavBar />
+        <CreateTransactionHeader />
+        {/* <TransactionForm
+          categorys={['군것질', '교통비', '회식비']}
+          payments={['카카오페이', '현금']}
+          onClick={onClick}
+          width="300px"
+          height="500px"
+        /> */}
+        <CreateTransactionMenu submit={onClick} cancel={onClick} />
+      </CenterContent>
     </>
   );
 };
