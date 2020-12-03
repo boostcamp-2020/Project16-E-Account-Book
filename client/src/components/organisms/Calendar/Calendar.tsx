8e2e29690cff5612b49e447fa6e3d8293f8cf753
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-12-03 16:08:05 +0900

    feat : numberOfMoneth 함수 구현

--- client/src/components/organisms/Calendar/Calendar.tsx
+++ client/src/components/organisms/Calendar/Calendar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import firstDayOfWeek from '@utils/firstDayOfWeek';
-import isLeafYear from '@utils/isLeapYear';
+// import isLeafYear from '@utils/isLeapYear';
+import numberOfMonth from '@utils/numberOfMonth';
 
 const Calendar = styled.div`
   border: 1px solid red;
 `;
 const calendar: React.FC = () => {
-  const date = firstDayOfWeek('2020-12');
-  const leafyear = isLeafYear(2020);
-  return <Calendar>{`${date}${leafyear}`}</Calendar>;
+  const day = firstDayOfWeek('2020-2');
+  const days = numberOfMonth('2020-2');
+  // const leafyear = isLeafYear(2020);
+  return <Calendar>{`${day}${days}`}</Calendar>;
 };
 
 export default calendar;
