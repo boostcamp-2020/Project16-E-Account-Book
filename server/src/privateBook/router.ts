4d93edefaf98c28c434ffbbc18c684d1631e3cdc
JunYoung <vnslt0152@gmail.com>
2020-12-07 23:54:38 +0900

    refactor : privateAccountbook 연,월 내역 조회 라우터 수정
    - get api이기 때문에 params로 data를 받을 수 있도록 하기 위한 url 형식 수정

--- server/src/privateBook/router.ts
+++ server/src/privateBook/router.ts
@@ -4,7 +4,7 @@ const Router = require('@koa/router');
 
 const router = new Router();
 
-router.get('/transactionList', Controller.getTransactionList);
+router.get('/transactionList/:accountbookId/:year/:month', Controller.getTransactionList);
 router.post('/transaction', Controller.createTransaction);
 
 export default router;
