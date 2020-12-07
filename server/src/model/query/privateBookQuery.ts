df40887e2d65db0407aeb157a5aa928af66cfd60
JunYoung <vnslt0152@gmail.com>
2020-12-07 23:26:06 +0900

    feat : privateAccountbook 내역 조회 service, query 추가
    - 연도, 월에 맞게 내역 조회를 위한 service, query문 추가

--- server/src/model/query/privateBookQuery.ts
+++ server/src/model/query/privateBookQuery.ts
@@ -3,6 +3,8 @@ const privateBookQuery = {
     'INSERT INTO private_accountbook (user_id, name, description, color) VALUES(?,?,?,?)',
   CREATE_PRIVATE_TRANSACTION:
     'INSERT INTO private_transaction (accountbook_id, category_id, payment_id, date, title, amount) VALUES(?,?,?,?,?,?)',
+  GET_PRIVATE_TRANSACTIONLIST:
+    'SELECT (pt.id, pt.date, py.name, ct.name , pt.title, pt.amount) FROM private_transaction as st JOIN category as ct ON st.category_id = ct.category_id LEFT OUTER JOIN payment as py on py.id = st.payment_id where st.accountbook_id = ? AND dateofyear(st.date) = ? AND dateofmonth(st.date) = ?',
 };
 
 export default privateBookQuery;
