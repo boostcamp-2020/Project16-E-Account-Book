90a305fb4c55f403ce11cad531001b5456ec960a
leejiwoo2002 <leejiwoo2002@gmail.com>
2020-12-08 03:20:31 +0900

    feat : API 구현 완료
    - 내림차순으로 [수입,지출] 쌍의 배열을 4개 반환
    - router와 service 코드 구현

--- server/src/socialBook/router.ts
+++ server/src/socialBook/router.ts
@@ -12,6 +12,6 @@ router.get('/transaction/:bookId/:date', Controller.getDailyTransaction);
 
 router.post('/transaction', Controller.createTransaction);
 
-router.get('/statistics/monthly', Controller.getMonthlyStatistics);
+router.get('/statistics/monthly/:bookId', Controller.getPastFourMonthStatistics);
 
 export default router;
