8af36b00279e0e0939d7a7ec98ba0250ebe23b3c
JunYoung <vnslt0152@gmail.com>
2020-12-08 00:08:31 +0900

    refactor : privateAccountbook 연,월 내역 조회 query 문 수정
    - 날짜가 빠른 순으로 정렬되어 가져오기 위한 ORDER BY 수행
    - 수입, 지출 여부도 가져오기 위한 assortment table과 JOIN 수행
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- server/src/model/query/privateBookQuery.ts
+++ server/src/model/query/privateBookQuery.ts
@@ -4,7 +4,7 @@ const privateBookQuery = {
   CREATE_PRIVATE_TRANSACTION:
     'INSERT INTO private_transaction (accountbook_id, category_id, payment_id, date, title, amount) VALUES(?,?,?,?,?,?)',
   GET_PRIVATE_TRANSACTIONLIST:
-    'SELECT pt.id, pt.date, py.name, ct.name , pt.title, pt.amount FROM private_transaction as pt JOIN category as ct ON pt.category_id = ct.id LEFT OUTER JOIN payment as py ON py.id = pt.payment_id WHERE pt.accountbook_id = ? AND year(pt.date) = ? AND month(pt.date) = ?',
+    'SELECT pt.id, pt.date, py.name, ct.name as category, pt.title, pt.amount, at.name as assortment FROM private_transaction as pt JOIN category as ct ON pt.category_id = ct.id JOIN assortment as at ON ct.assortment_id = at.id LEFT OUTER JOIN payment as py ON py.id = pt.payment_id WHERE pt.accountbook_id = ? AND year(pt.date) = ? AND month(pt.date) = ? ORDER BY pt.date',
 };
 
 export default privateBookQuery;
