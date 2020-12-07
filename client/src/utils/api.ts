25315dcd5239176ef885c1ef36ee638dcc1e7732
do02reen24 <do02reen24@gmail.com>
2020-12-07 17:40:45 +0900

    feat : category store 개발
    - init 개선 필요
    - 기초 함수 구현 및 reducer, action 틀 구축

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -3,3 +3,7 @@ import 'dotenv/config';
 export const GET_JWT = `${process.env.REACT_APP_BASE_URL}/login/`;
 
 export const GET_SOCIAL_BOOKS = `${process.env.REACT_APP_BASE_URL}/social/`;
+
+export const GET_INCOME_CATEGORY = `${process.env.REACT_APP_BASE_URL}/api/category/income`;
+
+export const GET_EXPENDITURE_CATEGORY = `${process.env.REACT_APP_BASE_URL}/api/category/expenditure`;
