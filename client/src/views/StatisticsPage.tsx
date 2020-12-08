d2e55565a2a449a8e8e3a6db7c02387806cdc809
do02reen24 <do02reen24@gmail.com>
2020-12-09 01:35:17 +0900

    feat : Statistics Page 초기 호출 데이터 설정 - 필요한 RootState 값 호출 및 선언 - 토글 버튼 선언 및 state 값 초기화

--- client/src/views/StatisticsPage.tsx
+++ client/src/views/StatisticsPage.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ToggleButton from '@atoms/div/ToggleButton';
+import MonthNav from '@molecules/MonthNav';
+import { useSelector } from 'react-redux';
+import { RootState } from '@reducers/rootReducer';
 
 const StatisticsPage: React.FC = () => {
+  const year = useSelector((state: RootState) => state.date.year);
+  const month = useSelector((state: RootState) => state.date.month);
+  const accountbookType = useSelector((state: RootState) => state.accountbook.type);
+  const accountbookId = useSelector((state: RootState) => state.accountbook.socialId);
+  const dateData = `${year}-${month}`;
+
+  const [isIncome, setIsIncome] = useState(true);
+  const [isExpenditure, setIsExpenditure] = useState(false);
+
+  useEffect(() => {
+    console.log(isIncome);
+    console.log(isExpenditure);
+    console.log(accountbookType);
+    console.log(accountbookId);
+  }, [dateData]);
+
+  const onClick = () => {
+    return true;
+  };
+
   return (
     <>
-      <p>statistics page</p>
+      <MonthNav />
+      <ToggleButton
+        leftButtonName="수입"
+        rightButtonName="지출"
+        leftCallback={setIsIncome}
+        rightCallback={setIsExpenditure}
+        onClick={onClick}
+      />
     </>
   );
 };
