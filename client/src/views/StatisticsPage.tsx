b493155f8551a0bb115f66ebecdcbd05287222d6
leejiwoo2002 <leejiwoo2002@gmail.com>
2020-12-09 01:25:23 +0900

    feat : socialbook 통계 페이지 차트 적용
    - FoutMonthStatistics 차트 적용
    - bookId를 store에서 받아와 axios에 사용

--- client/src/views/StatisticsPage.tsx
+++ client/src/views/StatisticsPage.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ColumFlexContainer from '@atoms/div/ColumnFlexContainer';
+import FourMonthStatistics from '@organisms/FourMonthStatistics';
+import { useSelector } from 'react-redux';
+import { getAxiosData } from '@utils/axios';
+import * as API from '@utils/api';
+import { RootState } from '@reducers/rootReducer';
 
 const StatisticsPage: React.FC = () => {
+  const accountbookId = useSelector((state: RootState) => state.accountbook.socialId);
+  const [fourMonthData, setFourMonthData] = useState([]);
+
+  console.log(accountbookId);
+  const initFourMonthData = async () => {
+    const master = await getAxiosData(`${API.GET_SOCIAL_FOUR_MONTH_STATISTICS}+${accountbookId}`);
+    setFourMonthData(master.data.reverse());
+  };
+
+  useEffect(() => {
+    initFourMonthData();
+  }, []);
+
   return (
-    <>
-      <p>statistics page</p>
-    </>
+    <ColumFlexContainer width="100%" alignItems="center">
+      <FourMonthStatistics data={fourMonthData} />
+    </ColumFlexContainer>
   );
 };
 
