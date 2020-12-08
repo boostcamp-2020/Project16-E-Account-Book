ee438061bde39f85a1ff65a667db72ebc3caecc0
JunYoung <vnslt0152@gmail.com>
2020-12-09 01:23:26 +0900

    refactor : TextArea 성능 확장 및 개선
    - value값을 사용하기 위한 value 속성 추가
    - 배경 색 조절을 위한 backgroundColor 속성 추가
    - 글씨 크기, 색깔을 조절하기 위한 속성들 추가 및 기본값 설정
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- client/src/components/atoms/textarea/TextArea/TextArea.tsx
+++ client/src/components/atoms/textarea/TextArea/TextArea.tsx
@@ -5,19 +5,55 @@ import myColor from '@theme/color';
 interface Props {
   width: string;
   height: string;
+  value?: string;
+  fontColor?: string;
+  backgroundColor?: string;
+  fontSize?: string;
+  onChange?: (event: any) => void;
 }
+
+const defaultProps = {
+  value: '',
+  backgroundColor: myColor.background.lightGray,
+  fontSize: '10pt',
+  fontColor: 'black',
+  onChange: undefined,
+};
 
 const TextArea = styled.textarea<Props>`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   resize: none;
   border: none;
-  background-color: ${myColor.background.lightGray};
+  background-color: ${(props) => props.backgroundColor};
   border-radius: 15px;
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => props.fontColor};
+  font-family: S-CoreDream-4Regular;
 `;
 
-const textArea: React.FC<Props> = ({ width, height }: Props) => {
-  return <TextArea width={width} height={height} />;
+const textArea: React.FC<Props> = ({
+  fontSize,
+  fontColor,
+  backgroundColor,
+  value,
+  width,
+  height,
+  onChange,
+}: Props) => {
+  return (
+    <TextArea
+      fontSize={fontSize}
+      fontColor={fontColor}
+      backgroundColor={backgroundColor}
+      onChange={onChange}
+      value={value}
+      width={width}
+      height={height}
+    />
+  );
 };
+
+textArea.defaultProps = defaultProps;
 
 export default textArea;
