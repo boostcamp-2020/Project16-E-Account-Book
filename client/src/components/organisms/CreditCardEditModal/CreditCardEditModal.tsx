e0123442c68dbe0793ee6c47e339c19ee1c5a1e8
JunYoung <vnslt0152@gmail.com>
2020-12-11 02:17:07 +0900

    refactor : createButtonClick 이벤트 수정
    생성 요청을 보내기 위한 AXIOS 함수 사용

--- client/src/components/organisms/CreditCardEditModal/CreditCardEditModal.tsx
+++ client/src/components/organisms/CreditCardEditModal/CreditCardEditModal.tsx
@@ -5,6 +5,8 @@ import Line from '@atoms/hr/Line';
 import myColor from '@theme/color';
 import styled from 'styled-components';
 import CreditCard from '@molecules/CreditCard';
+import * as Axios from '@utils/axios';
+import * as API from '@utils/api';
 
 const ScrollDiv = styled.div`
   overflow: scroll;
@@ -20,16 +22,12 @@ const ScrollDiv = styled.div`
 const CreditCardEditModal: React.FC = () => {
   const title = '결제 수단 관리';
 
-  const createButtonClick = async (data: any) => {
-    /* const data = {
-      name,
-      description,
-      usersList,
-      color,
-    };
-    const result = await Axios.postAxios(API.POST_CREATE_SOCIAL, data); */
-    console.log(data);
+  const createButtonClick = async (name: any) => {
+    const result = await Axios.postAxios(API.POST_PAYMENT, name);
+
+    console.log(result);
   };
+
   const cardNameList = ['신한카드', '하나카드', '카카오페이', '신한카드', '하나카드', '카카오페이'];
   const cardList = cardNameList.map((cardName) => <CreditCard name={cardName} />);
   return (
