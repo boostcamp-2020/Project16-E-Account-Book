a7d4aa84714b0d41b9ffa851b07aa505410ce705
do02reen24 <do02reen24@gmail.com>
2020-12-10 16:47:47 +0900

    refactor : mypage user image 및 디자인 개선
    - mypage의 디자인이 깨지는 부분을 수정함
    - mypage에 userimage를 store에서 가져와 설정해줌

--- client/src/components/organisms/MyPageUserMenu/MyPageUserMenu.tsx
+++ client/src/components/organisms/MyPageUserMenu/MyPageUserMenu.tsx
@@ -20,6 +20,10 @@ const ButtonContainer = styled.div`
   margin-left: auto;
 `;
 
+const Container = styled.div`
+  margin: 48px 20px 0px;
+`;
+
 const MyPageUserMenu: React.FC<Props> = ({ name, profile }: Props) => {
   const dispatch = useDispatch();
 
@@ -28,22 +32,24 @@ const MyPageUserMenu: React.FC<Props> = ({ name, profile }: Props) => {
     dispatch(logout());
   };
   return (
-    <RowFlexContainer margin="48px 20px 0px">
-      <UserImage link={profile} />
-      <ColumnFlexContainer alignItems="flex-start" margin="0 20px">
-        <P>
-          <Span>{name}</Span> 님
-        </P>
-        <P>안녕하세요</P>
-      </ColumnFlexContainer>
-      <ButtonContainer>
-        <Logout onClick={deleteJWT} color={myColor.primary.black}>
+    <Container>
+      <RowFlexContainer>
+        <UserImage link={profile} />
+        <ColumnFlexContainer alignItems="flex-start" margin="0 20px">
+          <P>
+            <Span>{name}</Span> 님
+          </P>
+          <P>안녕하세요</P>
+        </ColumnFlexContainer>
+        <ButtonContainer>
           <Link to="login">
-            <Span>로그아웃</Span>
+            <Logout onClick={deleteJWT} color={myColor.primary.black}>
+              <Span>로그아웃</Span>
+            </Logout>
           </Link>
-        </Logout>
-      </ButtonContainer>
-    </RowFlexContainer>
+        </ButtonContainer>
+      </RowFlexContainer>
+    </Container>
   );
 };
 
