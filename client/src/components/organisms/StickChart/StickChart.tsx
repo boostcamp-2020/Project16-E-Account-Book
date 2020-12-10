bbc8e2f14725799396469f5ea1d0f8fb221160e0
jekoo <pjkwprn@gmail.com>
2020-12-11 03:29:06 +0900

    feat : max four array 설정

--- client/src/components/organisms/StickChart/StickChart.tsx
+++ client/src/components/organisms/StickChart/StickChart.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import StickWithText from '@molecules/StickWithText';
 import myColor from '@theme/color';
+import getRandomKey from '@utils/random';
 
 interface Props {
   data: any;
@@ -25,49 +26,55 @@ const stickChart: React.FC<Props> = ({ data }: Props) => {
     myColor.statistic.expenditureThree,
     myColor.statistic.expenditureFour,
   ];
-  const incomeStick = data.income.map((ele, index) => {
-    const { name } = ele;
-    const money = Number(ele.money);
-    const percent = Number(ele.percent);
-    const inColor: any = incomeColor[index];
-    const outColor = 'white';
-    const height = '40px';
-    const width = '80%';
-    return (
-      <StickWithText
-        name={name}
-        money={money}
-        percent={percent}
-        inColor={inColor}
-        outColor={outColor}
-        height={height}
-        width={width}
-      />
-    );
-  });
+  const maxFourIncome = data.income.slice(0, 4);
+  const maxFourExpenditure = data.expenditure.slice(0, 4);
+  const incomeStick = () =>
+    maxFourIncome.map((ele, index) => {
+      const { name } = ele;
+      const money = Number(ele.money);
+      const percent = Number(ele.percent);
+      const inColor: any = incomeColor[index];
+      const outColor = 'white';
+      const height = '40px';
+      const width = '80%';
+      return (
+        <StickWithText
+          key={getRandomKey()}
+          name={name}
+          money={money}
+          percent={percent}
+          inColor={inColor}
+          outColor={outColor}
+          height={height}
+          width={width}
+        />
+      );
+    });
 
-  const expenditureStick = data.expenditure.map((ele, index) => {
-    const { name } = ele;
-    const money = Number(ele.money);
-    const percent = Number(ele.percent);
-    const inColor: any = expenditureColor[index];
-    const outColor = 'white';
-    const height = '40px';
-    const width = '80%';
-    return (
-      <StickWithText
-        name={name}
-        money={money}
-        percent={percent}
-        inColor={inColor}
-        outColor={outColor}
-        height={height}
-        width={width}
-      />
-    );
-  });
+  const expenditureStick = () =>
+    maxFourExpenditure.map((ele, index) => {
+      const { name } = ele;
+      const money = Number(ele.money);
+      const percent = Number(ele.percent);
+      const inColor: any = expenditureColor[index];
+      const outColor = 'white';
+      const height = '40px';
+      const width = '80%';
+      return (
+        <StickWithText
+          key={getRandomKey()}
+          name={name}
+          money={money}
+          percent={percent}
+          inColor={inColor}
+          outColor={outColor}
+          height={height}
+          width={width}
+        />
+      );
+    });
 
-  return <StickChart>{true ? incomeStick : expenditureStick}</StickChart>;
+  return <StickChart>{true ? incomeStick() : expenditureStick()}</StickChart>;
 };
 
 export default stickChart;
