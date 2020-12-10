5a684b422b11eaa7e9097bf1e264bf1c2ea3c0df
leejiwoo2002 <leejiwoo2002@gmail.com>
2020-12-11 00:15:07 +0900

    fix : 버튼이 공간을 차지하던 버그 수정
    - width, height container 삭제

--- client/src/components/organisms/NewTransactionButton/NewTransactionButton.tsx
+++ client/src/components/organisms/NewTransactionButton/NewTransactionButton.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import CreateButton from '@atoms/button/CreateButton';
 
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-`;
-
 const FloatRightBottomContainer = styled.div`
   position: fixed;
   right: 1.6em;
@@ -17,11 +11,11 @@ const FloatRightBottomContainer = styled.div`
 const NewTransactionButton: React.FC = () => {
   const link = '/accountbook/transaction/new';
   return (
-    <Container>
+    <>
       <FloatRightBottomContainer>
         <CreateButton link={link} />
       </FloatRightBottomContainer>
-    </Container>
+    </>
   );
 };
 
