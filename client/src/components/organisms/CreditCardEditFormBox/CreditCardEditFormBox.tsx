b67eeeb247ff833b1be2c3628e4e9c172abff458
JunYoung <vnslt0152@gmail.com>
2020-12-10 22:09:12 +0900

    feat : 결재 수단 이름 입력 변수 설정
    - 결재 수단 이름의 state를 관리하기 위해 useState 사용
    - 기존의 line atom을 사용하지 말고 input atom을 사용하도록 변경

--- client/src/components/organisms/CreditCardEditFormBox/CreditCardEditFormBox.tsx
+++ client/src/components/organisms/CreditCardEditFormBox/CreditCardEditFormBox.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
 import RowFlexContainer from '@atoms/div/RowFlexContainer';
 import LeftSmallText from '@atoms/p/LeftSmallText';
 import RoundShortButton from '@atoms/button/RoundShortButton';
-import Line from '@atoms/hr/Line';
+import Input from '@atoms/input/Input';
 
 const CreditCardEditFormBox: React.FC = () => {
+  const [name, setName] = useState('');
+
+  const nameChangeHandler = (event: any) => {
+    setName(event.target.value);
+  };
   return (
     <ColumnFlexContainer width="100%" margin="0 0 2rem 0" alignItems="baseline">
       <LeftSmallText>결재 수단 이름</LeftSmallText>
       <RowFlexContainer width="100%" margin="2rem 0 0 0 " justifyContent="space-between">
-        <Line widthPercent="100" margin="2rem 3rem 0 0" />
+        <Input width="70%" value={name} onChange={nameChangeHandler} />
         <RoundShortButton>등록</RoundShortButton>
       </RowFlexContainer>
     </ColumnFlexContainer>
