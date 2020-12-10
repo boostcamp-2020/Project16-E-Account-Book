a737c5a86bed56b4bf8a358934d697d8cb045c11
JunYoung <vnslt0152@gmail.com>
2020-12-11 00:02:24 +0900

    refactor : CreditCardEditModal 수정
    - 모달 내에서만 보여지기 위한 ScrollDiv 적용
    - createButtonClick 이벤트 등록
    - 따로 CreditCardEditCardList를 사용하지 않고 모달내에서 구현
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- client/src/components/organisms/CreditCardEditModal/CreditCardEditModal.tsx
+++ client/src/components/organisms/CreditCardEditModal/CreditCardEditModal.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import Modal from '@molecules/Modal';
-import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
 import CreditCardEditFormBox from '@organisms/CreditCardEditFormBox';
-import CreditCardEditCardList from '@organisms/CreditCardEditCardList';
 import Line from '@atoms/hr/Line';
 import myColor from '@theme/color';
+import styled from 'styled-components';
+import CreditCard from '@molecules/CreditCard';
+
+const ScrollDiv = styled.div`
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  display: flex;
+  flex-flow: column;
+  width: 100%;
+  height: 90%;
+`;
 
 const CreditCardEditModal: React.FC = () => {
   const title = '결제 수단 관리';
 
+  const createButtonClick = async (data: any) => {
+    /* const data = {
+      name,
+      description,
+      usersList,
+      color,
+    };
+    const result = await Axios.postAxios(API.POST_CREATE_SOCIAL, data); */
+    console.log(data);
+  };
+  const cardNameList = ['신한카드', '하나카드', '카카오페이', '신한카드', '하나카드', '카카오페이'];
+  const cardList = cardNameList.map((cardName) => <CreditCard name={cardName} />);
   return (
     <Modal title={title}>
-      <ColumnFlexContainer width="100%" justifyContent="space-between" margin="2rem 0 0 0">
-        <CreditCardEditFormBox />
-        <Line widthPercent="110" height="0.25rem" lineColor={myColor.primary.lightGray} />
-        <CreditCardEditCardList
-          cardNameList={[
-            '신한카드',
-            '하나카드',
-            '카카오페이',
-            '신한카드',
-            '하나카드',
-            '카카오페이',
-          ]}
-        />
-      </ColumnFlexContainer>
+      <CreditCardEditFormBox buttonEvent={createButtonClick} />
+      <Line widthPercent="110" height="0.25rem" lineColor={myColor.primary.lightGray} />
+      <ScrollDiv>{cardList}</ScrollDiv>
     </Modal>
   );
 };
