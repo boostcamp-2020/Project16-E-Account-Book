c392f77cf2d9b055bd15b378923c85d9cc648462
jekoo <pjkwprn@gmail.com>
2020-12-10 21:17:47 +0900

    feat : previous reducer 생성

--- client/src/actions/previous/type.ts
+++ client/src/actions/previous/type.ts
@@ -2,7 +2,7 @@ import { ActionProps } from '@interfaces/action';
 
 export const SET_PREVIOUS = 'previous/SET_PAYMENT' as const;
 
-export const setPayment = (previous: string): ActionProps => ({
+export const setPrevious = (previous: string): ActionProps => ({
   type: SET_PREVIOUS,
   payload: previous,
 });
