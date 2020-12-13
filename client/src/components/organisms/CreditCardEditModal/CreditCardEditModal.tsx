3584e24d4d90cc11423ccc8ac7d9d4e4e3c46bb8
JunYoung <vnslt0152@gmail.com>
2020-12-14 04:08:10 +0900

    refactor : 결재 수단 모달 저장 버튼 삭제
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- client/src/components/organisms/CreditCardEditModal/CreditCardEditModal.tsx
+++ client/src/components/organisms/CreditCardEditModal/CreditCardEditModal.tsx
@@ -5,13 +5,11 @@ import Line from '@atoms/hr/Line';
 import myColor from '@theme/color';
 import styled from 'styled-components';
 import CreditCard from '@molecules/CreditCard';
-import RowFlexContainer from '@atoms/div/RowFlexContainer';
 import * as Axios from '@utils/axios';
 import * as API from '@utils/api';
 import { setPayment } from '@actions/payment/type';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@reducers/rootReducer';
-import RoundShortButton from '@atoms/button/RoundShortButton';
 
 const ScrollDiv = styled.div`
   overflow: scroll;
@@ -62,9 +60,6 @@ const CreditCardEditModal: React.FC = () => {
       <CreditCardEditFormBox buttonEvent={createButtonClick} />
       <Line widthPercent="110" height="0.25rem" lineColor={myColor.primary.lightGray} />
       <ScrollDiv>{cardList}</ScrollDiv>
-      <RowFlexContainer justifyContent="flex-end">
-        <RoundShortButton>저장</RoundShortButton>
-      </RowFlexContainer>
     </Modal>
   );
 };
