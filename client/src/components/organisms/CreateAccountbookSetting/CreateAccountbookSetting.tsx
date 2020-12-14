e4d952c14fd38338d41b349121a3d14e00c0a351
JunYoung <vnslt0152@gmail.com>
2020-12-14 23:22:40 +0900

    refactor : 가계부 생성 setting 불필요한 props 제거
    - 불필요한 component import 제거 및 속성 제거

--- client/src/components/organisms/CreateAccountbookSetting/CreateAccountbookSetting.tsx
+++ client/src/components/organisms/CreateAccountbookSetting/CreateAccountbookSetting.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 // import styled from 'styled-components';
 import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
-import InviteAccountbookCard from '@molecules/InviteAccountbookCard';
 import ChangeAccountbookColorCard from '@molecules/ChangeAccountbookColorCard';
 
 interface Props {
-  links: string[];
   labelColor: string;
 }
 
-const CreateAccountbookSetting: React.FC<Props> = ({ links, labelColor }: Props) => {
+const CreateAccountbookSetting: React.FC<Props> = ({ labelColor }: Props) => {
   return (
     <ColumnFlexContainer margin="2rem 0 1rem 0">
-      <InviteAccountbookCard links={links} />
       <ChangeAccountbookColorCard labelColor={labelColor} />
     </ColumnFlexContainer>
   );
