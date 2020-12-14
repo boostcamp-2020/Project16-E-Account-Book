4f70a0a6b92d2bc4861de64d804aae8bc0bdeac0
JunYoung <vnslt0152@gmail.com>
2020-12-15 02:10:38 +0900

    feat : CreateAccountbookColorModal 구현
    - 가계부 색깔 변경을 위한 색깔 변경 모달 구현
    - scrollView를 적용하여 모달 안에 content들이 존재하게 구현
    - 이차원 배열로 component 생성
    - 첫번째 줄에 첫번째 요소는 현재 사용중인 색깔이 위치하도록 구현

---
+++ client/src/components/organisms/CreateAccountbookColorModal/CreateAccountbookColorModal.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Modal from '@molecules/Modal';
+import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
+import RowFlexContainer from '@atoms/div/RowFlexContainer';
+import ColorLabel from '@atoms/div/ColorLabel';
+import TextButton from '@atoms/button/TextButton';
+import styled from 'styled-components';
+// import myColor from '@theme/color';
+/* import * as Axios from '@utils/axios';
+import * as API from '@utils/api';
+import { setPayment } from '@actions/payment/type';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@reducers/rootReducer'; */
+
+interface Props {
+  currentColor: string;
+}
+
+const ScrollDiv = styled.div`
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  display: flex;
+  flex-flow: column;
+  width: 100%;
+  height: 90%;
+`;
+
+const ButtonDiv = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 2rem;
+  justify-content: flex-end;
+`;
+
+const CreateAccountbookColorModal: React.FC<Props> = ({ currentColor }: Props) => {
+  const title = '색상 변경하기';
+
+  const colorList = [
+    [currentColor, '#FFBB00', '#ABF200', '#1DDB16'],
+    ['#00D8FF', '#0054FF', '#5F00FF', '#FF00DD'],
+    ['#FFA7A7', '#FAED7D', '#B7F0B1', '#B5B2FF'],
+    ['#000000', '#D5D5D5', '#008299', '#D4F4FA'],
+    ['#BDBDBD', '#C4B73B', '#670000', '#FFB4B4'],
+    ['#EAEAEA', '#D1B2FF', '#FAF4C0', '#D3FFFF'],
+    ['#FFFF6C', '#14899D', '#4B0CA5', '#845E00'],
+  ];
+
+  return (
+    <Modal title={title}>
+      <ButtonDiv>
+        <TextButton>확인</TextButton>
+      </ButtonDiv>
+      <ScrollDiv>
+        <ColumnFlexContainer width="90%">
+          {colorList.map((color) => {
+            return (
+              <RowFlexContainer justifyContent="space-between" margin="0 0 2rem 0">
+                <ColorLabel backgroundColor={color[0]} />
+                <ColorLabel backgroundColor={color[1]} />
+                <ColorLabel backgroundColor={color[2]} />
+                <ColorLabel backgroundColor={color[3]} />
+              </RowFlexContainer>
+            );
+          })}
+        </ColumnFlexContainer>
+      </ScrollDiv>
+    </Modal>
+  );
+};
+
+export default CreateAccountbookColorModal;
