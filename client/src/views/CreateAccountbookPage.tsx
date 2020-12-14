c1115e9a86a1fb913ad423e416d13f84252117b6
JunYoung <vnslt0152@gmail.com>
2020-12-14 23:23:53 +0900

    # feat : 가계부 생성 페이지 links props 제거
    refactor : 가계부 생성페이지에서 불필요한 link props 제거

--- client/src/views/CreateAccountbookPage.tsx
+++ client/src/views/CreateAccountbookPage.tsx
@@ -11,14 +11,12 @@ import styled from 'styled-components';
 import * as API from '@utils/api'; */
 
 const CreateAccountbookPage: React.FC = () => {
-  const createButtonClick = async (data: any) => {
-    /* const data = {
+  const createButtonClick = async (name: any, description: any) => {
+    const data = {
       name,
       description,
-      usersList,
-      color,
     };
-    const result = await Axios.postAxios(API.POST_CREATE_SOCIAL, data); */
+    // const result = await Axios.postAxios(API.POST_CREATE_SOCIAL, data);
     console.log(data);
   };
 
@@ -38,14 +36,7 @@ const CreateAccountbookPage: React.FC = () => {
           backgroundColor={backgroundColor}
         />
         <SettingContainer>
-          <CreateAccountbookSetting
-            labelColor="blue"
-            links={[
-              'https://avatars2.githubusercontent.com/u/46099115?s=460&u=1e04610d430875d8189d2b212b8c2d9fc268b9db&v=4',
-              'https://avatars3.githubusercontent.com/u/55074799?s=460&u=2f70319c2f55ba5e26db060ba21d66a9cab35732&v=4',
-              'https://avatars2.githubusercontent.com/u/50297117?s=460&u=2ddc78ef0045b75f6fb405f1763304a7481d46e4&v=4',
-            ]}
-          />
+          <CreateAccountbookSetting labelColor={backgroundColor} />
         </SettingContainer>
       </CenterContent>
     </>
