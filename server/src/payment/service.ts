76136f3d78589ec1d66a82f00d287dec752680db
JunYoung <vnslt0152@gmail.com>
2020-12-14 22:33:12 +0900

    feat : delete user payment를 위한 service 구현
    # <Why>를 위한 <What>
    # 로그인을 위한 OAuth api 구현
    # 객체 의존성 역전을 위한 프로토콜 추상화
    
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # --------------------------------------------------------------
    # 메세지는 한글로 작성
    # 타입 첫 문자는 소문자로 작성
    # 제목의 끝은 명사형으로 작성
    # 제목 끝에 마침표(.) 금지
    # 본문은 "어떻게" 보다 "무엇을", "왜"를 설명

--- server/src/payment/service.ts
+++ server/src/payment/service.ts
@@ -26,3 +26,24 @@ export const getPaymentIdByName = async (name: string) => {
   const result = await sql(query.GET_PAYMENT_ID_BY_NAME, [name]);
   return result;
 };
+
+export const deletePayment = async (id: number) => {
+  await sql(query.DELETE_PAYMENTS, [id]);
+};
+
+export const deleteUserPayment = async (id: number) => {
+  await sql(query.DELETE_USER_PAYMENTS, [id]);
+};
+
+export const deleteRelatedSocialTransaction = async (userId: number, paymentId: number) => {
+  await sql(query.DELETE_SOCIAL_TRANSACTION_BY_USERID_PAYMENTID, [userId, paymentId]);
+};
+
+export const deleteRelatedPrivateTransaction = async (accountbookId: number, paymentId: number) => {
+  await sql(query.DELETE_PRIVATE_TRANSACTION_BY_PAYMENTID, [accountbookId, paymentId]);
+};
+
+export const geyPaymentIdByPk = async (id: number) => {
+  const result = await sql(query.GET_PAYMENT_ID_BY_PK, [id]);
+  return result[0].payment_id;
+};
