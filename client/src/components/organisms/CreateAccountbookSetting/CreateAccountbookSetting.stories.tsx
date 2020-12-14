6738996a6e994f055221b2d4eae8b91ccab6a8bf
JunYoung <vnslt0152@gmail.com>
2020-12-14 23:21:29 +0900

    refactor : 가계부 생성 setting stories 불필요한 props 제거
    - 사용하지 않는 links 제거를 위한 코드 수정

--- client/src/components/organisms/CreateAccountbookSetting/CreateAccountbookSetting.stories.tsx
+++ client/src/components/organisms/CreateAccountbookSetting/CreateAccountbookSetting.stories.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import CreateAccountbookSetting from './CreateAccountbookSetting';
 
 interface Props {
-  links: string[];
   labelColor: string;
 }
 
@@ -24,10 +23,5 @@ createAccountbookSetting.story = {
 };
 
 createAccountbookSetting.args = {
-  links: [
-    'https://avatars2.githubusercontent.com/u/46099115?s=460&u=1e04610d430875d8189d2b212b8c2d9fc268b9db&v=4',
-    'https://avatars3.githubusercontent.com/u/55074799?s=460&u=2f70319c2f55ba5e26db060ba21d66a9cab35732&v=4',
-    'https://avatars2.githubusercontent.com/u/50297117?s=460&u=2ddc78ef0045b75f6fb405f1763304a7481d46e4&v=4',
-  ],
   labelColor: 'blue',
 };
