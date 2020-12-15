22dc58a4c0588cde79a151987f335328667cb285
JunYoung <vnslt0152@gmail.com>
2020-12-15 17:44:38 +0900

    feat : themes colorList 추가
    - 가계부 생성 페이지 색깔 목록을 한정시키기 위한 colorList 추가

---
+++ client/src/theme/colorList.ts
@@ -0,0 +1,11 @@
+const colorList = [
+  ['#FF0000', '#FFBB00', '#ABF200', '#1DDB16'],
+  ['#00D8FF', '#0054FF', '#5F00FF', '#FF00DD'],
+  ['#FFA7A7', '#FAED7D', '#B7F0B1', '#B5B2FF'],
+  ['#000000', '#D5D5D5', '#008299', '#D4F4FA'],
+  ['#BDBDBD', '#C4B73B', '#670000', '#FFB4B4'],
+  ['#EAEAEA', '#D1B2FF', '#FAF4C0', '#D3FFFF'],
+  ['#FFFF6C', '#14899D', '#4B0CA5', '#845E00'],
+];
+
+export default colorList;
