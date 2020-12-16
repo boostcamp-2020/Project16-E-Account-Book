6abf86e9321cdf2d9d8cc83dadaa8eb8c0520a36
JunYoung <vnslt0152@gmail.com>
2020-12-16 21:49:54 +0900

    feat : ColorLabelWithCheck 구현
    - ColorLabel에 체크된 형태를 사용하기 위한 ColorLabelWithCheck 구현
    - SVG를 ColorLabel children으로 넘겨주어 구현

---
+++ client/src/components/molecules/ColorLabelWithCheck/ColorLabelWithCheck.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ColorLabel from '@atoms/div/ColorLabel';
+import CheckSVG from '@svg/check.svg';
+import colorUtils from '@utils/color';
+
+interface Props extends SizeProps {
+  backgroundColor: string;
+  buttonEvent?: (data) => void;
+}
+
+interface SizeProps {
+  size?: string;
+}
+
+const defaultProps = {
+  size: '1.5rem',
+  buttonEvent: undefined,
+};
+
+const ColorLabelWithCheck: React.FC<Props> = ({ backgroundColor, ...args }: Props) => {
+  return (
+    <ColorLabel backgroundColor={backgroundColor} {...args}>
+      <CheckSVG width="70%" height="70%" fill={colorUtils.getFontColor(backgroundColor)} />
+    </ColorLabel>
+  );
+};
+
+ColorLabelWithCheck.defaultProps = defaultProps;
+
+export default ColorLabelWithCheck;
