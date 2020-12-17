704112db2cec31267fab4c4a431a7bb38e8c6654
jekoo <pjkwprn@gmail.com>
2020-12-17 23:33:00 +0900

    fix : build error 수정

--- client/src/views/NotificationPage.tsx
+++ client/src/views/NotificationPage.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import CenterContent from '@molecules/CenterContent';
 import PreparationBox from '@molecules/PreparationBox';
 import TopNavBar from '@organisms/TopNavBar';
+import myColor from '@theme/color';
 
 const NotificationPage: React.FC = () => {
   return (
     <CenterContent>
       <TopNavBar />
-      <PreparationBox>현재 준비중인 서비스 입니다!</PreparationBox>
+      <PreparationBox color={myColor.primary.black}>현재 준비중인 서비스 입니다!</PreparationBox>
     </CenterContent>
   );
 };
