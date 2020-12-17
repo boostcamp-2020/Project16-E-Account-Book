95feb44dfaf1337c2c120b04024b71c351a113c3
jekoo <pjkwprn@gmail.com>
2020-12-17 11:24:57 +0900

    feat : preparationBox molecules 구현

--- client/src/components/molecules/PreparationBox/PreparationBox.tsx
+++ client/src/components/molecules/PreparationBox/PreparationBox.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import ColumnFlexContainer from '@atoms/div/ColumnFlexContainer';
+import Caution from '@svg/caution.svg';
+import CautionMent from '@atoms/p/CenterNormalText';
 
-const preparationBox: React.FC = () => {
+interface Props {
+  children: React.ReactChild;
+}
+
+const SVG = styled.img`
+  border: 0;
+`;
+
+const preparationBox: React.FC<Props> = ({ children }: Props) => {
   return (
-
+    <ColumnFlexContainer>
+      <SVG src={Caution} alt="caution" />
+      <CautionMent>{children}</CautionMent>
+    </ColumnFlexContainer>
   );
 };
 export default preparationBox;
