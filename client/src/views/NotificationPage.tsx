bbeb21def274ee4f7f4271e6c87a794aa331df9d
do02reen24 <do02reen24@gmail.com>
2020-12-17 21:37:11 +0900

    feat : 알림 페이지 준비중인 서비스입니다 멘트 추가
    - 알림 페이지가 구현되지 않아 준비중인 서비스 입니다 라는 멘트를 추가해둠

--- client/src/views/NotificationPage.tsx
+++ client/src/views/NotificationPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CenterContent from '@molecules/CenterContent';
+import PreparationBox from '@molecules/PreparationBox';
 import TopNavBar from '@organisms/TopNavBar';
 
 const NotificationPage: React.FC = () => {
   return (
     <CenterContent>
       <TopNavBar />
+      <PreparationBox>현재 준비중인 서비스 입니다!</PreparationBox>
     </CenterContent>
   );
 };
