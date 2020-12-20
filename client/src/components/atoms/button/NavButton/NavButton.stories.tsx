4f09564e0dfdbc4f1e29b64a82995b8ed15f9201
JunYoung <vnslt0152@gmail.com>
2020-12-20 20:41:17 +0900

    refactor : NavButton stories controls 추가

--- client/src/components/atoms/button/NavButton/NavButton.stories.tsx
+++ client/src/components/atoms/button/NavButton/NavButton.stories.tsx
@@ -4,27 +4,54 @@ import NavButton from './NavButton';
 export default {
   title: 'Atoms/button/NavButton',
   component: NavButton,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    width: { control: 'text' },
+    height: { control: 'text' },
+    iconColor: { control: 'color' },
+    name: { control: 'name' },
+  },
 };
 
-export const bellButton = (): JSX.Element => {
-  const moveUrl = '/';
-  const name = 'bell';
-  const iconColor = '#7392FF';
+interface Props extends backgroundColorProps {
+  name: string;
+  moveUrl: string;
+  iconColor?: string;
+  width?: string;
+  height?: string;
+  onClick?: () => void;
+}
 
-  return <NavButton moveUrl={moveUrl} name={name} iconColor={iconColor} />;
+interface backgroundColorProps {
+  backgroundColor?: string;
+  width?: string;
+  height?: string;
+}
+
+export const bellButton = ({ ...args }: Props): JSX.Element => {
+  return <NavButton {...args} />;
 };
 
-export const settingButton = (): JSX.Element => {
-  const moveUrl = '/';
-  const name = 'setting';
-  const iconColor = '#7392FF';
-  return <NavButton moveUrl={moveUrl} name={name} iconColor={iconColor} />;
+export const settingButton = ({ ...args }: Props): JSX.Element => {
+  return <NavButton {...args} />;
 };
 
 bellButton.story = {
   name: 'bellButton',
 };
 
+bellButton.args = {
+  moveUrl: '/',
+  name: 'bell',
+  iconColor: '#7392FF',
+};
+
 settingButton.story = {
   name: 'settingButton',
+};
+
+settingButton.args = {
+  moveUrl: '/',
+  name: 'setting',
+  iconColor: '#7392FF',
 };
