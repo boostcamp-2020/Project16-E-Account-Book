2b4aea0bb0261c7a3b8b1d6317542e331f156aaa
JunYoung <vnslt0152@gmail.com>
2020-12-20 20:42:02 +0900

    refactor : TwoBarGraph stories controls 추가

--- client/src/components/atoms/graph/TwoBarGraph/TwoBarGraph.stories.tsx
+++ client/src/components/atoms/graph/TwoBarGraph/TwoBarGraph.stories.tsx
@@ -5,16 +5,33 @@ import TwoBarGraph from './TwoBarGraph';
 export default {
   title: 'Atoms/graph/TwoBarGraph',
   component: TwoBarGraph,
+  argTypes: {
+    maxHeight: { control: 'number' },
+    income: { control: 'number' },
+    expend: { control: 'number' },
+  },
 };
 
-export const twoBarGraph = (): JSX.Element => {
+interface Props {
+  maxHeight: number;
+  income: number;
+  expend: number;
+}
+
+export const twoBarGraph = ({ ...args }: Props): JSX.Element => {
   return (
     <RowFlexContainer width="100px" height="400px">
-      <TwoBarGraph maxHeight={170000} income={40000} expend={20000} />
+      <TwoBarGraph {...args} />
     </RowFlexContainer>
   );
 };
 
 twoBarGraph.story = {
   name: 'TwoBarGraph',
+};
+
+twoBarGraph.args = {
+  maxHeight: 170000,
+  income: 40000,
+  expend: 20000,
 };
